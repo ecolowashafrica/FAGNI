9b923d11d3fca11f06cca82f0da6f92f308ba275
ecolowashafrica <ecolowash.africa@gmail.com>
2025-09-29 22:27:46 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -36,7 +36,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "fagni_backend.urls"
+ROOT_URLCONF = "FAGNI.urls"
 
 TEMPLATES = [
     {
@@ -54,7 +54,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "fagni_backend.wsgi.application"
+WSGI_APPLICATION = "FAGNI.wsgi.application"
 
 # --- Base de données ---
 # Local par défaut (SQLite). En prod, configure Postgres via variables d'env.
