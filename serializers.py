6d349a70e9c7bb1a198e2f8ae750a8ab4930382a
ecolowashafrica <ecolowash.africa@gmail.com>
2025-09-29 21:28:54 +0200

    Update serializers.py

--- serializers.py
+++ serializers.py
@@ -3,26 +3,16 @@ from rest_framework import serializers
 from .models import Client, Partner, Order, Payment, Incident
 
 class ClientSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = "__all__"
+    class Meta: model = Client; fields = "__all__"
 
 class PartnerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Partner
-        fields = "__all__"
+    class Meta: model = Partner; fields = "__all__"
 
 class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = "__all__"
+    class Meta: model = Order; fields = "__all__"
 
 class PaymentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Payment
-        fields = "__all__"
+    class Meta: model = Payment; fields = "__all__"
 
 class IncidentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Incident
-        fields = "__all__"
+    class Meta: model = Incident; fields = "__all__"
