123c200651f9314742d7037966af0f93fd5f4f58
ecolowashafrica <ecolowash.africa@gmail.com>
2025-09-29 10:50:22 +0200

    Update asgi.py

--- asgi.py
+++ asgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.asgi import get_asgi_application
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fagni_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 application = get_asgi_application()
